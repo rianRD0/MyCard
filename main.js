fb2cd51e1d0f0673bec16cf019f4424e36651c31
Rian Guedes <98765215+rianRD0@users.noreply.github.com>
2022-09-22 16:06:36 -0300

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+const linksSocialMedia = {
+    github: "rianRD0",
+    instagram: "rianguedes",
+    twitter: "RianGuedes10",
+    facebook: "rian.guedes.3",
+    youtube: "UCMCVbwTAsFUpUyWL_6T7gmw"
+}
+
+function changeSocialMediaLinks(){
+    for(let li of socialLinks.children){
+        const social = (li.getAttribute('class'))
+        
+        li.children[0].href = `https://${social}.com/${linksSocialMedia[social]}`
+
+    }
+}
+changeSocialMediaLinks()
+
+function getGitHubProfileInfos(){
+    const url = `https://api.github.com/users/${linksSocialMedia.github}`
+    fetch(url)
+    .then(response => response.json() )
+    .then( data => {
+        userName.textContent = data.name
+        userBio.textContent = data.bio
+        userLink.href = data.html_url
+        userAvatar.src = data.avatar_url
+        userLogin.textContent = data.login
+
+    })
+}
+getGitHubProfileInfos()
